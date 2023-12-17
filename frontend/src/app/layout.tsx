e666f30bbe4ca6c2305c18082a640cd76599d1a9
RichQ-hub <74640863+RichQ-hub@users.noreply.github.com>
2023-12-17 23:48:23 +1100

    Setup next app

---
+++ frontend/src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type { Metadata } from 'next'
+import { Fira_Sans } from 'next/font/google'
+import './globals.css'
+
+const fira = Fira_Sans({
+  weight: ['400', '500', '700']
+})
+
+export const metadata: Metadata = {
+  title: 'Interex',
+  description: 'A social platform where communities of like-minded people share interests.',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={fira.className}>{children}</body>
+    </html>
+  )
+}
