d36b89f5b90239064747e0f2880170f75bcec103
RichQ-hub <74640863+RichQ-hub@users.noreply.github.com>
2023-12-19 17:49:24 +1100

    Setup nextjs pages

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Fira_Sans } from 'next/font/google'
 import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs';
 
 const fira = Fira_Sans({
   weight: ['400', '500', '700']
@@ -17,8 +18,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={fira.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={fira.className}>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
